2765d8b294ec645418d428b39a950c98addebd8e
juan <juan@BYRON>
2016-05-16 18:34:07 -0500

    Avance

---
+++ NetBeansProjects/ANIMALES_GITHUB/src/animales_github/Persona.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package animales_github;
+
+/**
+ *
+ * @author juan
+ */
+public class Persona {
+    private String Nombre;
+    private Boolean Sexo;
+    private int edad;
+
+    public Persona(String Nombre, Boolean Sexo, int edad) {
+        this.Nombre = Nombre;
+        this.Sexo = Sexo;
+        this.edad = edad;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
