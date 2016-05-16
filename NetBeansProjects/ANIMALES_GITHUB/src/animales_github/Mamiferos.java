babc3c7c7ab00043fabd158ca972043c677eac32
juan <juan@BYRON>
2016-05-16 18:27:54 -0500

    Inicio de Proyecto

---
+++ NetBeansProjects/ANIMALES_GITHUB/src/animales_github/Mamiferos.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package animales_github;
+
+/**
+ *
+ * @author BYRON
+ */
+public class Mamiferos {
+    private String Tipo;
+    private String Nombre;
+    private String Caracteristica;
+    private String Color;
+    private String Alimentacion;
+
+    public Mamiferos(String Tipo, String Nombre, String Caracteristica, String Color, String Alimentacion) {
+        this.Tipo = Tipo;
+        this.Nombre = Nombre;
+        this.Caracteristica = Caracteristica;
+        this.Color = Color;
+        this.Alimentacion = Alimentacion;
+    }
+
+
+
+   
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ NetBeansProjects/ANIMALES_GITHUB/src/animales_github/Reptiles.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package animales_github;
+
+/**
+ *
+ * @author BYRON
+ */
+public class Reptiles extends Mamiferos {
+
+    public Reptiles(String Tipo, String Nombre, String Caracteristica, String Color, String Alimentacion) {
+        super(Tipo, Nombre, Caracteristica, Color, Alimentacion);
+    }
+    private String Tipo_Reproduccion;
+}
